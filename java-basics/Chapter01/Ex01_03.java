f91cb9f48f16b6e61a7088af3dce800d956c53ce
tanmoychakraborty00001-a11y <tanmoy.chakraborty00001@gmail.com>
2025-11-22 11:30:52 +0000

    Add Exercise Ex01_03

--- java-basics/Chapter01/Ex01_03.java
+++ java-basics/Chapter01/Ex01_03.java
@@ -1,4 +1,9 @@
 public class Ex01_03 {
-
+   public static void main(String[] args) {
+       System.out.println("Hello, World!");
+       System.out.println("Simplication of the mathematical operations: " + (5+3*2+(8/4)));
+       System.out.printf("Simplification of the mathematical operations: %d", (5 + 3*2 + (8/4)));
+       System.out.printf("Let's checking formatted output with floating point numbers: %.2f", 5.0/3);
+   }
     
 }
